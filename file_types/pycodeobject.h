e07718afab1bb0ac6959cd51a4c8b4b2de44f4d0
davidkirakosyan <david.kirakosyan.41@mail.ru>
2021-11-25 22:22:53 +0300

    Implemented simple PyCodeObject. Needs fixing

---
+++ file_types/pycodeobject.h
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include "file_types/file_types.h"
+
+#ifndef PYCODEOBJECT_H
+#define PYCODEOBJECT_H
+
+class PyCodeObject : public PyObject{
+public:
+    std::vector<uint8_t> code;
+    std::vector<ptr> consts;
+    std::vector<const char*> names;
+    std::vector<const char*> varnames;
+    PyCodeObject() = default;
+};
+#endif //PYCODEOBJECT_H
